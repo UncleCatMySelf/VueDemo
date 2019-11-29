2f6c5f7842647d2ef9f928075e9acb3211bcbb5d
MySelf <zhupeijie_java@126.com>
2019-11-29 22:41:09 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <p>{{name}}</p>
+    <button @click="update">更新数据</button>
+    <HelloWorld msg="Welcome to Your Vue.js App" v-if="show"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return{
+      show:true,
+      name:'welcome to vue'
+    }
+  },
+  beforeCreate(){
+    console.log('beforeCreate')
+    console.log(this.$el)
+    console.log(this.$data)
+  },
+  created(){
+    console.log('created')
+    console.log(this.$el)
+    console.log(this.$data)
+  },
+  beforeMount(){
+    console.log('beforeMount')
+    console.log(this.$el)
+    console.log(this.$data)
+  },
+  mounted(){
+    console.log('mounted')
+    console.log(this.$el)
+    console.log(this.$data)
+  },
+  beforeUpdate(){
+    console.log('beforeUpdate')
+  },
+  updated(){
+    console.log('updated')
+  },
+  beforeDestroy(){
+    console.log('beforeDestroy')
+  },
+  destroyed(){
+    console.log('destroyed')
+  },
+  methods:{
+    update(){
+      this.name = 'jack';
+      this.show = false;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
